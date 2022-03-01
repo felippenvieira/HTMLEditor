ad8838dafeda25a7add74b4b60e7abc4e945e78b
Felippe <felippe_nvieira@hotmail.com>
2022-03-01 09:53:07 -0300

    Screen drawing

--- HTMLEditor/Menu.cs
+++ HTMLEditor/Menu.cs
@@ -10,7 +10,32 @@ namespace HTMLEditor
             Console.BackgroundColor = ConsoleColor.Blue;
             Console.ForegroundColor = ConsoleColor.Black;
 
+            DrawScreen(30, 10);
+        }
 
+        public static void DrawScreen(int col, int lines)
+        {
+            StartEndLine(col);
+
+            for (int y = 0; y <= lines; y++)
+            {
+                Console.Write("|");
+                for (int i = 0; i <= col; i++)
+                    Console.Write(" ");
+                Console.Write("|");
+                Console.Write("\n");
+            }
+
+            StartEndLine(col);
+        }
+
+        public static void StartEndLine(int col)
+        {
+            Console.Write("+");
+            for (int i = 0; i <= col; i++)
+                Console.Write("-");
+            Console.Write("+");
+            Console.Write("\n");
         }
     }
 }
